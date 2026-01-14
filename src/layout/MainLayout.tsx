21e0eb371156fe7893bdf649497d841ef5dd5c99
eziz-tajiev <eziztajiev@gmail.com>
2026-01-14 16:30:39 +0500

    update path

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,13 +1,13 @@
 import { Outlet } from 'react-router-dom'
-import { Header } from '../components/Header/Header'
-import { Footer } from '../components/Footer/Footer'
+import { Header } from '../components/Header'
+import { Footer } from '../components/Footer'
 
 export const MainLayout = () => {
   return (
     <>
       <Header />
 
-      <main className='main-content'>
+      <main className="main-content">
         <Outlet />
       </main>
 
