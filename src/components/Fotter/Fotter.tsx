624732c5230ff6b714774ed5f55862840a53cb6f
eziz-tajiev <eziztajiev@gmail.com>
2026-01-14 12:20:07 +0500

    add icons

---
+++ src/components/Fotter/Fotter.tsx
@@ -0,0 +1,23 @@
+import { FotterSubscribe } from '../FotterSubscribe/FotterSubscribe'
+import styles from './fotter.module.css'
+
+export const Footer = () => {
+  return (
+    <footer>
+      <FotterSubscribe />
+      <div className={styles.section}>
+        <div className={styles.container}>
+          <div>
+            <img src="/images/logo.png" width={259} height={70} alt="logo" />
+            <div>
+              <img />
+              <img />
+              <img />
+              <img />
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  )
+}
