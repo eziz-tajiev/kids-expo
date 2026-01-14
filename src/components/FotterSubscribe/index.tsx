d40d2d03899d309e8000c55a2f9f9d4a474cbad5
eziz-tajiev <eziztajiev@gmail.com>
2026-01-14 16:24:57 +0500

    replace file to index

---
+++ src/components/FotterSubscribe/index.tsx
@@ -0,0 +1,11 @@
+import styles from './fotterSubscribe.module.css'
+
+export const FotterSubscribe = () => {
+  return (
+    <div className={styles.container}>
+      <span>Подпишитесь на новости:</span>
+      <input className={styles.input} placeholder="Placeholder" />
+      <button className={styles.button}>Подписаться</button>
+    </div>
+  )
+}
